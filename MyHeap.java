778b36b0366fd415eff49647c6e2756c971bc9f0
Taejoon Kim <tkim00@stuy.edu>
2019-04-21 20:52:18 -0400

    tested heapify and working. fixed pushDown so it works properly during heapify. Not too sure if the array will contain null elements as its precondition.

--- MyHeap.java
+++ MyHeap.java
@@ -8,23 +8,24 @@ public class MyHeap {
   private static void pushDown(int[]data,int size,int index) {
     int c1 = 2*index+1;
     int c2 = 2*index+2;
-    if (c1 > size  //there may be some edge cases that I have to cover
-                  //this might happen when data[index] is passed towards bottom right
-    || (data[index] > data[c1] && data[index] > data[c2])) {
-      return;
-    } else {
-      if (c1 != size) {
-        if (data[c1] > data[c2]) {
-          swap(data, c1, index);
-          pushDown(data, size, c1);
-        } else {
-          swap(data, c2, index);
-          pushDown(data, size, c2);
-        }
-      } else { //edge case when the value is being passed down to the last corner.
+    if (c1 != size) {
+      if (c1 > size
+      || (data[index] >= data[c1] && data[index] >= data[c2])) {
+        return;
+      }
+      if (data[c1] > data[c2]) {
         swap(data, c1, index);
         pushDown(data, size, c1);
+      } else {
+        swap(data, c2, index);
+        pushDown(data, size, c2);
       }
+    } else {
+      if (data[index] >= data[c1]) {
+        return;
+      }
+      swap(data, c1, index);
+      pushDown(data, size, c1);
     }
   }
 
@@ -35,6 +36,12 @@ public class MyHeap {
     } else {
       swap(data, p, index);
       pushUp(data, p);
+    }
+  }
+
+  public static void heapify (int[] data) {
+    for (int i = (data.length-2)/2; i >= 0; i--) {
+      pushDown(data, data.length-1, i);
     }
   }
 }
